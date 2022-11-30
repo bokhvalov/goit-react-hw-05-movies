afc7c692589dc99f8c1280d2eaa267ea42acb011
bokhvalov <Dev_6>
2022-11-30 19:09:04 +0100

    change path of imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Cast from 'components/Cast/Cast';
 import MovieDetails from 'components/MovieDetails/MovieDetails';
 import Reviews from 'components/Reviews/Reviews';
-import Home from 'pages/Home/Home';
-import Movies from 'pages/Movies/Movies';
+import Home from 'pages/home/Home';
+import Movies from 'pages/movies/Movies';
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/Layout/SharedLayout';
 
